dbc5cca46569b9befa82e70c55068716d59cfe78
Bocharick <pomazannn@gmail.com>
2018-10-31 12:14:29 +0700

    task_07

--- Lesson_02/task_07.py
+++ Lesson_02/task_07.py
@@ -5,3 +5,16 @@
 1+2+...+n = n(n+1)/2, где n – любое натуральное число.
 
 """
+
+n = int(input("Введите натуральное число n: "))
+
+left = 0
+right = n * (n + 1) // 2
+
+for i in range(n+1):
+    left += i
+
+if left == right:
+    print(True)
+else:
+    print(False)
